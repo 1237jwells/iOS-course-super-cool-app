f80d6f1cc94155d2a5186f2d418e247cda6d77e6
Jordan Wells <jordanwells123456789@hotmail.com>
2018-08-08 16:57:23 -0600

    Conditionals Commit

---
+++ Contents.swift
@@ -0,0 +1,93 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+//Conditional #1
+
+var isMyHouseOnFire: Bool = false
+
+var anotherBool = true
+
+if (isMyHouseOnFire){
+    print("Someone Get Me Some Water")
+}
+else {
+    print("Someone Get some Fire for my house!")
+}
+
+var result =  true == true
+result = true == false
+result = false == false
+
+
+var accountTotal = 300.33
+var newCallOfDutyGame = 59.99
+
+if (accountTotal >= newCallOfDutyGame)
+{
+    print("I just purchased the game!")
+}
+else
+{
+    print("I dont have enough money!")
+}
+
+var name = "Jackie"
+
+if name == "Jack"
+{
+    print("Your name is Jack")
+}
+else
+{
+    print("Your name is not Jack")
+}
+
+var numberA = 25
+var numberB = 100
+var numberC = 45
+var numberD = 50
+
+var finalNumber = 100
+
+if numberA == finalNumber {
+    print(numberA)
+} else if numberB == finalNumber {
+    print(numberB)
+} else if numberC == finalNumber {
+    print(numberC)
+} else {
+    print("None of the values were equal to finalNumber")
+}
+
+
+
+
+
+//Conditionals #2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
